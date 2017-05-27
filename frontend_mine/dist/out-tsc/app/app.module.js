6b28150f2bf0942efe1c877aaae4738e57a0b4ea
arahansa <arahansa@naver.com>
2017-05-28 08:58:26 +0900

    앵귤러인메모리 붙인 뒤

--- frontend_mine/dist/out-tsc/app/app.module.js
+++ frontend_mine/dist/out-tsc/app/app.module.js
@@ -7,12 +7,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
-import { DashboardComponent } from "./dashboard.component";
-import { AppRoutingModule } from "./app-routing.module";
 var AppModule = (function () {
     function AppModule() {
     }
@@ -23,17 +27,17 @@ AppModule = __decorate([
         imports: [
             BrowserModule,
             FormsModule,
+            HttpModule,
+            InMemoryWebApiModule.forRoot(InMemoryDataService),
             AppRoutingModule
         ],
         declarations: [
             AppComponent,
-            HeroDetailComponent,
             DashboardComponent,
-            HeroesComponent
+            HeroDetailComponent,
+            HeroesComponent,
         ],
-        providers: [
-            HeroService
-        ],
+        providers: [HeroService],
         bootstrap: [AppComponent]
     })
 ], AppModule);
